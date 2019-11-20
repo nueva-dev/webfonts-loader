e0d3c9c1d7603643813b863cde0d9578d78caaf2
Manuela <manuela@nmedias.net>
2019-11-20 09:58:23 +0100

    updated file path for scss file

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ module.exports = function (content) {
 
   // Spit out SCSS file to same path as CSS file to easily use mixins (scssFile must be true)
   if (fontConfig.scssFile === true) {
-    generatorOptions.cssDest = path.resolve(this.context, fontConfig.dest, fontConfig.fontName + '.scss');
+    generatorOptions.cssDest = path.resolve(this.context, fontConfig.fontName + '.scss');
   }
 
   // svgicons2svgfont stuff
